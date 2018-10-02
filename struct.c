55e80bebbd60a6eba5e70822f639e004b340f16b
Alex Liu <alexliu@Alexs-MacBook-Pro.local>
2018-10-02 15:35:51 -0400

    randomlocation and takeover

--- struct.c
+++ struct.c
@@ -3,15 +3,34 @@
 #include <time.h>
 
 
-struct foo{int x; char* p; };
+struct location{char *name; char *leader; };
 
-time_t t = time(NULL);
-//printf("current time: %lu\n \n", t );
+void rand_location() {
+  char *array[5];
+  array[0] = "America";
+  array[1] = "Hogwarts";
+  array[3] = "Stuyvesant";
+  time_t t = time(NULL);
+  srand(t);
+  printf("%s\n", array[rand() % 3]);
+}
 
-srand(t);
-
-
+struct location take_over_location(struct location f, char *new_leader) {
+  f.leader = new_leader;
+  return f;
+}
+void print_location(struct location f) {
+  printf("%s is led by %s\n", f.name, f.leader);
+}
 
 int main() {
+  struct location p;
+  p.name = "Galaxy";
+  p.leader = "Darth Vader";
+  print_location(p);
+  p = take_over_location(p, "Thanos");
+  print_location(p);
+
+  rand_location();
   return 0;
 }
