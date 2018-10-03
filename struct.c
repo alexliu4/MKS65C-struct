2b79e1894da6de40f91b4ef9d6b34d4a1f690d10
Brian <hlee5@stuy.edu>
2018-10-03 15:35:46 -0400

    rename

--- struct.c
+++ struct.c
@@ -5,32 +5,35 @@
 
 struct location {
     char name[30];
-    char leader[30];
+    char villian[30];
     unsigned int population;
 };
 
 char names[10][30] = 
   {"America", "Hogwarts", "Stuyvesant", "Nakatomi Plaza", "Area 51", "Middle Earth", "Gondor", "Gotham", "Asgard", "Brooklyn 99 Precint"};
-char leaders[10][30] = 
-  {"Darth Vader", "Donald Trump", "Mr. Konstantinovich", "Bernie Sanders", "Sauron", "Hans Gruber", "Joker",
-  "Loki", "Bane", "Chief Deputy Wuntch"};
+char villians[10][30] = 
+  {"Darth Vader", "Donald Trump", "Mr. Konstantinovich", "Bernie Sanders", "Sauron", "Hans Gruber", "Joker", "Loki", "Bane", "Chief Deputy Wuntch"};
 
 struct location rand_location() {
   struct location temp;
   strcpy(temp.name, names[rand() % 10]);
-  strcpy(temp.leader, leaders[rand() % 10]); 
+  strcpy(temp.villian, villian[rand() % 10]); 
   temp.population = rand() % 10000;
   return temp;
 }
 
-void conquer(struct location *p, char *new_leader) {
-  strcpy(p->leader, new_leader);
+void conquer(struct location *p, char *new_villian) {
+  strcpy(p->villian, new_villian);
   p->population *= ((float)rand() / RAND_MAX);
+}
+
+void rename_location(struct location *p) {
+  p->name[0] = 'A';
 }
 
 void print_location(struct location loc) {
   printf("%s is controlled by %s and has a population of %d\n",
-          loc.name, loc.leader, loc.population);
+          loc.name, loc.villian, loc.population);
 }
 
 int main() {
@@ -40,6 +43,7 @@ int main() {
   conquer(&p, "Mr. Peanut");
   print_location(p);
   conquer(&p, "Paul McCartney"); 
+  rename_location(&p);
   print_location(p);
   return 0;
 }
